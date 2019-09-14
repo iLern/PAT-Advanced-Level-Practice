7dc133ab87ea109852b227c5b72dc141a55abe03
iLern <416138794@qq.com>
2019-09-14 22:45:31 +0800

    day2

--- 1086.cpp
+++ 1086.cpp
@@ -1,64 +1,78 @@
 #include <iostream>
 #include <string>
-#include <stack>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-const int MAXN = 30 + 5;
+const int MAXN = 30 + 10;
 
 vector<int> ans;
 struct Node {
     int id;
-    Node *lc, *rc;
+    Node* lc, *rc;
 
-    Node() {
+    Node(int id) : id(id) {
         lc = rc = nullptr;
     }
 
     void postOrder() {
         if (lc) lc->postOrder();
         if (rc) rc->postOrder();
+
         ans.push_back(this->id);
     }
-} nodes[MAXN];
+};
 
+vector<int> prd, inrd;
+//l1 r1->prd l2 r2->inrd
+Node* build(int l1, int r1, int l2, int r2) {
+    if (l1 > r1) return nullptr;
+
+    Node *root = new Node(prd[l1]);
+
+    int tmp = l2;
+    while (inrd[tmp] != root->id) tmp++;
+
+    int num = tmp - l2;
+    root->lc = build(l1 + 1, l1 + num, l2, tmp - 1);
+    root->rc = build(l1 + num + 1, r1, tmp + 1, r2);
+
+    return root;
+}
 
 int main() {
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        nodes[i].id = i;
-    }
-
-    // int cnt = 1;
     stack<int> st;
-    Node *root = nullptr;
     for (int i = 0; i < n * 2; i++) {
         string op;
         cin >> op;
-        // cout << op << " " << id << endl;
+
         if (op == "Push") {
             int id;
             cin >> id;
-
-            if (st.empty()) root = nodes + id;
-            else {
-                Node* fa = nodes + st.top();
-                if (fa->lc) fa->rc = nodes + id;
-                else fa->lc = nodes + id;
-            }
-
             st.push(id);
-            // cnt++;
-        } else if (op == "Pop") {
+            prd.push_back(id);
+        } else {
+            int tmp = st.top();
+            inrd.push_back(tmp);
+
             st.pop();
         }
     }
 
-    root->postOrder();
+#ifdef DBG
+    for (auto x : prd) cout << x << " ";
+    cout << endl;
+    for (auto x : inrd) cout << x << " ";
+    cout << endl;
+#endif
 
+    Node* root = build(0, n - 1, 0, n - 1);
+    root->postOrder();
+    
     for (int i = 0; i < ans.size(); i++) {
         if (i != ans.size() - 1) cout << ans[i] << " ";
         else cout << ans[i] << endl;
