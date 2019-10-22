2cbb92158861a11f99c3f01c3784dbe98369d431
iLern <416138794@qq.com>
2019-10-22 15:11:00 +0800

    link list

--- 1052.cpp
+++ 1052.cpp
@@ -1,6 +1,45 @@
 #include <cstdio>
+#include <algorithm>
+
+const int MAXN = 100000 + 10;
+
+struct Node {
+    int address, data, next;
+    bool flag;
+
+    Node() : address(0), data(0), next(-1), flag(false) {}
+} nodes[MAXN];
 
 int main() {
-    
+    int n, head;
+    scanf("%d%d", &n, &head);
+
+    for (int i = 0; i < n; i++) {
+        int address, data, next;
+        scanf("%d%d%d", &address, &data, &next);
+
+        nodes[address].address = address;
+        nodes[address].data = data;
+        nodes[address].next = next;
+    }
+
+    int cnt = 0;
+    for (int v = head; v != -1; v = nodes[v].next) {
+        nodes[v].flag = true;
+        cnt++;
+    }
+
+    if (cnt == 0) {
+        printf("0 -1\n");
+    } else {
+        std::sort(nodes, nodes + MAXN, [](Node a, Node b)->bool{return !a.flag || !b.flag ? a.flag > b.flag : a.data < b.data;});
+        printf("%d %05d\n", cnt, nodes[0].address);
+        for (int i = 0; i < cnt; i++) {
+            printf("%05d %d ", nodes[i].address, nodes[i].data);
+            if (i != cnt - 1) printf("%05d\n", nodes[i + 1].address);
+            else printf("-1\n");
+        }
+    }
+
     return 0;
 }
--- 1074.cpp
+++ 1074.cpp
@@ -1,44 +1,62 @@
 #include <cstdio>
+#include <vector>
+#include <algorithm>
 
 const int MAXN = 100000 + 10;
 
 struct Node {
-    int data;
-    Node *next;
+    int address, data, next;
+    bool flag;
 
-    Node() {
-        data = 0;
-        next = nullptr;
-    }
+    Node() : address(0), data(0), next(-1), flag(false) {}
 } nodes[MAXN];
 
-void reverse(Node *begin, Node *end) {
-    
-}
-
 int main() {
-    int first, n, k;
-    scanf("%d%d%d", &first, &n, &k);
+    int head, n, k;
+    scanf("%d%d%d", &head, &n, &k);
 
     for (int i = 0; i < n; i++) {
         int address, data, next;
         scanf("%d%d%d", &address, &data, &next);
-        nodes[address].data = data;
 
-        if (next != -1) nodes[address].next = &nodes[next];
-        else nodes[address].next = nullptr;
+        nodes[address].address = address;
+        nodes[address].data = data;
+        nodes[address].next = next;
     }
 
+    std::vector<Node> vec;
+
     int cnt = 0;
-    Node *begin = &nodes[first], *end = &nodes[first];
-    while (end) {
-        if (cnt == 2) reverse(begin, end);
+    for (int v = head; v != -1; v = nodes[v].next) {
         cnt++;
-        
-        if (cnt == 3) {
-            cnt = 0;
-            begin = end;
-        }
+        nodes[v].flag = true;
+
+        if (cnt <= k) vec.push_back(nodes[v]);
+    }
+
+    std::reverse(vec.begin(), vec.end());
+
+    // printf("-----------\n");
+    // for (auto x : vec) {
+    //     printf("%05d %d %05d\n", x.address, x.data, x.next);
+    // }
+
+    int tmp = vec[0].next;
+    for (int i = 0; i < vec.size(); i++) {
+        if (i < vec.size() - 1) vec[i].next = vec[i + 1].address;
+        else vec[i].next = tmp;
+    }
+
+    // printf("-----------\n");
+    for (auto x : vec) {
+        if (x.next != -1) printf("%05d %d %05d\n", x.address, x.data, x.next);
+        else printf("%05d %d -1\n", x.address, x.data);
+    }
+
+    for (int v = tmp; v != -1; v = nodes[v].next) {
+        printf("%05d %d ", nodes[v].address, nodes[v].data);
+        if (nodes[v].next != -1) printf("%05d\n", nodes[v].next);
+        else printf("-1\n");
     }
     return 0;
 }
